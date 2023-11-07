5e1f3f98e9c1c3e4448f4535ba940897fd768748
Jonathan Ashby <66997727+Im2UrRight@users.noreply.github.com>
2023-11-06 18:12:15 -0600

    Update script.js
    - renamed apiContainer to apodContainer
    - optimized todayFormat function
    - adjusted fetch request and constructor to handle errors

--- script.js
+++ script.js
@@ -5,24 +5,16 @@
 //date must be in YYYY-MM-DD format, Must be after 1995-06-16
 //test date 11/23/2010
 const api_key = 'L6f1Y4xOae6pw2Fj3zNg9PwEt3E6F1WD72FYC4cU'
-const apiContainer = document.querySelector('.apod')
+const apodContainer = document.querySelector('.apod')
 const dateInput = document.querySelector('#date')
 const apodContainer = document.querySelector('.apod')
 const rdmBtn = document.querySelector('#rdm')
 const srchBtn = document.querySelector('#srch')
-let today = todayFormat()
 
-//set max for date input
-dateInput.max = today
-//toISOString always uses UTC time so can't use this unless i account for that
 function todayFormat() {
-    var now = new Date();
-    var yyyy = now.getFullYear();
-    var m = now.getMonth() + 1;
-    var d = now.getDate();
-    var mm = m < 10 ? '0' + m : m;
-    var dd = d < 10 ? '0' + d : d;
-    return ''+yyyy+'-'+mm+'-'+dd;
+  const now = new Date();
+  now.setUTCHours(0, 0, 0, 0);
+  return now.toISOString().slice(0, 10);
 }
 
 // to get a randome date
@@ -35,53 +27,56 @@ function randomDate(minDate, maxDate) {
 
 //use fetch api to get info
 async function fetchAPOD(date) {
-  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${date}`)
-    .then(data => data.json())
-    .catch(error => {
-      console.error(`Error communicating with NASA APOD server: ${error.message}`)
-    })
-    constructor(response)
+  try {
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${date}`);
+    if (!response.ok) {
+      throw new Error(`Error communicating with NASA APOD server: ${response.statusText}`);
+    }
+    const data = await response.json();
+    constructor(data);
+  } catch (error) {
+    console.error(`Error communicating with NASA APOD server: ${error.message}`);
   }
-
+}
 
 //constructor for api
 function constructor(apod) {
-  clearElement(apiContainer)
-  
-  let localDate = new Date(apod.date).toLocaleString()
-  localDate = localDate.slice(0, localDate.indexOf(','))
-  const imageContainer = document.createElement('div')
-  const apodImage = new Image()
-  const apodImgErr = document.createElement('p')
-  const apodCaption = document.createElement('figcaption')
-  const apodExpl = document.createElement('p')
-  const apodDate = document.createElement('span')
+  clearElement(apodContainer);
+  const imageContainer = document.createElement('div');
+  const apodImage = new Image();
+  const apodImgErr = document.createElement('p');
+  const apodCaption = document.createElement('figcaption');
+  const apodExpl = document.createElement('p');
+  const apodDate = document.createElement('span');
 
   //internals
-  imageContainer.classList.add('apod_image')
-  apodImage.src = apod.url
-  apodCaption.innerText = `${apod.title}. `
-  apodDate.innerText = localDate
-  apodDate.classList.add('date')
-  apodExpl.innerText = apod.explanation
-  apodExpl.classList.add('expl')
+  imageContainer.classList.add('apod_image');
+  apodImage.src = apod.url;
+  apodCaption.innerText = `${apod.title}.`;
+  apodDate.innerText = localDate;
+  apodDate.classList.add('date');
+  apodExpl.innerText = apod.explanation;
+  apodExpl.classList.add('expl');
   
   //append
-  apodCaption.appendChild(apodDate)
-  apiContainer.appendChild(imageContainer)
-  imageContainer.appendChild(apodImage)
-  imageContainer.appendChild(apodImgErr)
-  imageContainer.appendChild(apodCaption)
-  apiContainer.appendChild(apodExpl)
-
-  //event listeners
-
+  apodCaption.appendChild(apodDate);
+  apodContainer.appendChild(imageContainer);
+  imageContainer.appendChild(apodImage);
+  imageContainer.appendChild(apodImgErr);
+  imageContainer.appendChild(apodCaption);
+  apodContainer.appendChild(apodExpl);
+  
+  // Event listeners
+  apodImage.onload = function () {
+    apodImgErr.style.display = 'none';
+  };
   //errors
-  apodImage.onerror = function() {
-    apodImage.display = 'hidden'
-    apodImgErr.innerText = "Sorry, this image was lost amoungst the stars"
-  } 
+  apodImage.onerror = function () {
+    apodImage.style.display = 'none';
+    apodImgErr.innerText = "Sorry, this image was lost among the stars";
+  };
 }
+
 //clear element
 function clearElement(element) {
   while (element.firstChild) {
@@ -97,7 +92,8 @@ srchBtn.addEventListener('click', e => {
 
 rdmBtn.addEventListener('click', e => {
   e.preventDefault()
-  fetchAPOD(randomDate('1995-06-16', today))
-})
+  fetchAPOD(randomDate('1995-06-16', todayFormat()));
+});
 
-fetchAPOD(today)
+// Initial fetch
+fetchAPOD(todayFormat())
